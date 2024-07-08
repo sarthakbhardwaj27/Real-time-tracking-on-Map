10eef9615c4c8033fe4911290515ffb1010919d9
SarthakBhardwajCode <85218961+sarthakbhardwaj27@users.noreply.github.com>
2024-07-09 02:25:02 +0530

    added markers functionality and exchange of coordinates in FE and BE

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on("connection", function(socket){
+  socket.on("send-location",function(data){
+    io.emit("receive-location",{id: socket.id, ...data})
+  });
+  socket.on("disconnect", function(){
+    io.emit("user-disconnected", socket.id);
+  })
   console.log("connected")
 })
 
